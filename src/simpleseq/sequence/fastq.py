3b3d169b825e518a4097443f2875cff651c0f6a3
AJC <mail@ambrosejcarr.com>
2016-01-26 23:49:07 -0500

    debugging

--- src/simpleseq/sequence/fastq.py
+++ src/simpleseq/sequence/fastq.py
@@ -152,7 +152,8 @@ def merge_fastq(merge_function, fout, genomic, barcode=None):
     genomic: fastq containing genomic data
     barcode: fastq containing barcode data
     """
-    if not os.path.isdir(fout):
+    directory, filename = os.path.split(fout)
+    if not os.path.isdir(directory):
         os.makedirs(fout, exist_ok=True)
     genomic = FastqReader(genomic)
     barcode = FastqReader(barcode)
